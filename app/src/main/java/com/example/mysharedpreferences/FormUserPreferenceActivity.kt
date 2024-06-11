c3fae2ee931eb4e9210fdf2afd10f7bc1354f611
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-06-11 19:11:46 +0700

    update shared pref 1

---
+++ app/src/main/java/com/example/mysharedpreferences/FormUserPreferenceActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mysharedpreferences
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormUserPreferenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_user_preference)
+    }
+}
